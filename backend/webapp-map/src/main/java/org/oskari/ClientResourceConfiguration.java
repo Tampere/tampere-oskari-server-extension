0baab38a86c2a5a4e26b88b275f612209845b442
Tuomas Riihimäki <tuomas.riihimaki@ubigu.fi>
2025-03-28 10:26:48 +0200

    build backend and frontend from same repo.

---
+++ backend/webapp-map/src/main/java/org/oskari/ClientResourceConfiguration.java
@@ -0,0 +1,24 @@
+package org.oskari;
+
+import fi.nls.oskari.util.PropertyUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ClientResourceConfiguration implements WebMvcConfigurer {
+
+    public ClientResourceConfiguration(){
+        System.out.println("Loading oskari-override.properties");
+        PropertyUtil.loadProperties("/oskari-docker.properties");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("Registering /Oskari as static resource location");
+        registry.addResourceHandler("/Oskari/**")
+                .addResourceLocations("/Oskari/")
+                .setCachePeriod(60*60*8) // Cache static files for 8 hours
+        ;
+    }
+}
