1fa8fe299cc5783bdd4b2dac0e4bab87c0094b97
Tuomas Riihimäki <tuomas.riihimaki@ubigu.fi>
2025-04-01 23:25:18 +0300

    Add login filter for username login from tre-internal net

--- backend/server-extension/src/main/java/fi/nls/oskari/spring/security/TreIntraDbloginFilterConfiguration.java
+++ backend/server-extension/src/main/java/fi/nls/oskari/spring/security/TreIntraDbloginFilterConfiguration.java
@@ -3,11 +3,15 @@ package fi.nls.oskari.spring.security;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.spring.SpringEnvHelper;
-import org.springframework.context.annotation.Bean;
+import fi.nls.oskari.spring.security.database.OskariAuthenticationProvider;
+import fi.nls.oskari.spring.security.database.OskariAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,14 +23,47 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Profile("TreUsernameFilter")
+//@Configuration
 @Configuration
-public class TreIntraDbloginFilterConfiguration {
+@EnableWebSecurity
+@Order(1)
+public class TreIntraDbloginFilterConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final Logger logger = LogFactory.getLogger(TreIntraDbloginFilterConfiguration.class);
-    private final SpringEnvHelper envHelper;
+    private final SpringEnvHelper env;
 
+    @Autowired
     public TreIntraDbloginFilterConfiguration(SpringEnvHelper envHelper) {
-        this.envHelper = envHelper;
+        this.env = envHelper;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        logger.info("Configuring database login with Tampere intra filter");
+        /*
+         * We want to permitAll since guests can access everything. What makes the login work is that:
+         * - loginProcessingUrl is the login form action url
+         * - passwordParameter/usernameParameter matches the login form fields
+         * - loginPage might not be needed since we permit all URLs
+         */
+        http.authenticationProvider( new OskariAuthenticationProvider() );
+        http.headers().frameOptions().disable();
+
+        // 3rd party cookie blockers don't really work with cookie based CSRF protection on embedded maps.
+        // Configure nginx to attach SameSite-flag to cookies instead.
+        http.csrf().disable();
+
+        // IMPORTANT! Only antMatch for processing url, otherwise SAML security filters are passed even if both are active
+        http.formLogin()
+                .loginProcessingUrl(env.getLoginUrl())
+                .passwordParameter(env.getParam_password())
+                .usernameParameter(env.getParam_username())
+                .failureHandler(new OskariLoginFailureHandler("/?loginState=failed"))
+                .successHandler(new OskariAuthenticationSuccessHandler())
+                .loginPage("/");
+
+        logger.warn("Adding Tampere internal network filter to loginurl: " + env.getLoginUrl());
+        http.antMatcher(env.getLoginUrl()).addFilterBefore(treIpFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
 
     private static final Filter treIpFilter = new OncePerRequestFilter() {
@@ -44,11 +81,5 @@ public class TreIntraDbloginFilterConfiguration {
 
     };
 
-    @Bean
-    public SecurityFilterChain addTampereInternalNetworkFilter(HttpSecurity http) throws Exception {
-        logger.warn("Adding Tampere internal network filter");
-        http.antMatcher(envHelper.getLoginUrl()).addFilterBefore(treIpFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }
 
 }
