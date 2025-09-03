684ceb33a3a5799a18d81a5b764e365d36ccf100
Tuomas Riihimäki <tuomas.riihimaki@ubigu.fi>
2025-09-03 15:49:32 +0300

    Add login filter page

--- backend/server-extension/src/main/java/fi/nls/oskari/spring/security/TreIntraDbloginFilterConfiguration.java
+++ backend/server-extension/src/main/java/fi/nls/oskari/spring/security/TreIntraDbloginFilterConfiguration.java
@@ -5,6 +5,7 @@ import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.spring.SpringEnvHelper;
 import fi.nls.oskari.spring.security.database.OskariAuthenticationProvider;
 import fi.nls.oskari.spring.security.database.OskariAuthenticationSuccessHandler;
+import fi.nls.oskari.util.PropertyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.Filter;
@@ -21,8 +23,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Profile("TreUsernameFilter")
+@Profile("TreLoginWhitelistConfig")
 //@Configuration
 @Configuration
 @EnableWebSecurity
@@ -31,10 +36,13 @@ public class TreIntraDbloginFilterConfiguration extends WebSecurityConfigurerAda
 
     private static final Logger logger = LogFactory.getLogger(TreIntraDbloginFilterConfiguration.class);
     private final SpringEnvHelper env;
+    private final List<IpAddressMatcher> ipLoginWhitelist;
 
     @Autowired
     public TreIntraDbloginFilterConfiguration(SpringEnvHelper envHelper) {
         this.env = envHelper;
+        ipLoginWhitelist = Arrays.stream(PropertyUtil.get("login.ip.whitelist", "").trim().split(",")).map(IpAddressMatcher::new).collect(Collectors.toList());
+
     }
 
     protected void configure(HttpSecurity http) throws Exception {
@@ -45,7 +53,7 @@ public class TreIntraDbloginFilterConfiguration extends WebSecurityConfigurerAda
          * - passwordParameter/usernameParameter matches the login form fields
          * - loginPage might not be needed since we permit all URLs
          */
-        http.authenticationProvider( new OskariAuthenticationProvider() );
+        http.authenticationProvider(new OskariAuthenticationProvider());
         http.headers().frameOptions().disable();
 
         // 3rd party cookie blockers don't really work with cookie based CSRF protection on embedded maps.
@@ -66,20 +74,27 @@ public class TreIntraDbloginFilterConfiguration extends WebSecurityConfigurerAda
 
     }
 
-    private static final Filter treIpFilter = new OncePerRequestFilter() {
+    private final Filter treIpFilter = new OncePerRequestFilter() {
 
         @Override
         protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
             logger.info("Executing filter for login requests to URI " + request.getRequestURI() + " from IP: " + request.getRemoteAddr());
-            if (!request.getRemoteAddr().startsWith("10.")) {
-                logger.warn("Logging not allowed for IP: " + request.getRemoteAddr());
-                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Login forbidden from this network");
-                return;
+            if (isAllowedIp(request)) {
+                filterChain.doFilter(request, response);
             }
-            filterChain.doFilter(request, response);
+            logger.warn("Logging not allowed for IP: " + request.getRemoteAddr());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Login forbidden from this network");
         }
 
     };
 
 
+    public boolean isAllowedIp(HttpServletRequest request) {
+        for (IpAddressMatcher ipAddressMatcher : ipLoginWhitelist) {
+            if (ipAddressMatcher.matches(request)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
